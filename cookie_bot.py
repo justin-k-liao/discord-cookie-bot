946a95061d4f87c5298f3c5170af5d208b147d4f
Justin <liaoj9@gmail.com>
2021-09-02 00:32:27 -0700

    first commit

---
+++ cookie_bot.py
@@ -0,0 +1,19 @@
+import pyautogui, asyncio
+
+def do_daily(): 
+    pyautogui.write(str('>>roulette '+ str(betAmount) + ' color:red'))
+    pyautogui.press('enter')
+
+def gib_cookie(): 
+    return NotImplemented
+
+if __name__ == '__main__': 
+    do_daily()
+    gib_cookie() 
+
+# 1) does cookiefarming 
+# 2) does daily 
+# 3) check kud w/ opencv OCR
+# 4) check shop 
+# 5) purchase from shop if requirements are met 
+# 	a) find and click the button required if we have enough kud
