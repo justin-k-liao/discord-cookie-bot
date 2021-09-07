bba56dc21f7a6858a7129b59cbbc4946a124fac1
justin-k-liao <liaoj9@gmail.com>
2021-09-07 10:07:10 -0700

    added print statements

--- cookie_bot.py
+++ cookie_bot.py
@@ -8,6 +8,7 @@ async def do_daily(): #22 hour cooldown 3600 seconds in 1 hour
         pyautogui.press('enter')
         sleep_time = random.randint(22,24)
         logging.info(f"daily {sleep_time}")
+        print(f"daily sleep {sleep_time} hours")
         await asyncio.sleep(3600*sleep_time)
 
 async def gib_cookie(): #58 minute cooldown 60 seconds to minute
@@ -19,6 +20,7 @@ async def gib_cookie(): #58 minute cooldown 60 seconds to minute
             pyautogui.press('enter')
             sleep_time = random.randint(60, 75)
             logging.info(f"cookie {sleep_time}")
+        print(f"cookie sleep {sleep_time}minutes")
         await asyncio.sleep(60*sleep_time)
 
 async def main(): 
@@ -35,7 +37,9 @@ async def main():
 
 asyncio.run(main())
 # 1) does cookiefarming 
+    #done 
 # 2) does daily 
+#   done 
 #   2b) needs to check the result of each with python ocr 
 # 3) check kud w/ opencv OCR
 # 4) check shop 
